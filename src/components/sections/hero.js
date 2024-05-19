bd23886f8df1250019dbf3ecc2e2e4e9898bdcaa
Ernst Kandombo <51041541+Ernstkandombo@users.noreply.github.com>
2024-05-19 22:03:06 +0200

    ads

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -3,13 +3,17 @@ import { Button } from '../ui/button'
 
 export default function hero() {
     return (
-        <section className="text-center py-20 bg-white">
-            <h1 className="text-4xl font-bold mb-4">Security you can rely on. A name you can trust.</h1>
-            <p className="text-lg mb-8">A VPN from the trusted pioneer in internet privacy.</p>
-            <Button className="bg-black text-white py-3 px-6 rounded-full mb-8">Join the Waitlist</Button>
-            <p className="text-sm text-gray-600">We currently offer VPN in the US, UK, Canada, New Zealand, Singapore, and Malaysia.</p>
-            <p className="text-sm text-gray-600">Available on: <span className="font-bold">Windows, Android, iOS</span>. Mac & Linux coming soon.</p>
-            <img src="https://placehold.co/400x400" alt="VPN illustration" className="mt-8 mx-auto max-w-full h-auto" />
+        <section className="flex justify-center items-center py-20 gap-6ß bg-white">
+            <div>
+                <h1 className="text-4xl font-bold mb-4 max-w-xs">Security you can rely on. A name you can trust.</h1>
+                <p className="text-lg mb-8">A VPN from the trusted pioneer in internet privacy.</p>
+                <Button className="bg-black text-white py-3 px-6 mb-8">Join the Waitlist</Button>
+                <p className="text-sm text-gray-600  max-w-sm">We currently offer VPN in the Namibia, South Africa, Botswana, Angola, Zambia, and Zimbwabe.</p>
+                <p className="text-sm text-gray-600 py-2">Available on: <span className="font-bold">Windows, Android, iOS</span>. Mac & Linux coming soon.</p>
+            </div>
+            <div>
+                <img src="https://placehold.co/400x400" alt="VPN illustration" className="mt-8 mx-auto max-w-full h-auto" />
+            </div>
         </section>
     )
 }
--- src/components/sections/nav.js
+++ src/components/sections/nav.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
 import { Button } from '../ui/button'
+import Image from 'next/image'
 
 export default function nav() {
     return (
-        <header className="flex flex-wrap justify-between items-center p-6 bg-white shadow">
+        <header className="flex flex-wrap justify-between items-center py-2 shadow">
             <div className="container flex justify-between items-center">
-                <div className="text-xl font-bold"> EK VPN </div>
+                <div className=" flex gap-2 text-xl justify-center items-center font-bold">
+                    <Image src="/assets/icons8-kerio-control-480.png" width={100} height={100} alt="logo" className="w-10 h-10" />
+                    <h1>EK <span className="text-[#FC7219]">VPN</span></h1>
+                </div>
                 <div className="flex space-x-2 gap-4 mt-4 lg:mt-0">
                     <Button variant="outline">Join the Waitlist</Button>
                     <Button href="#">Sign In</Button>
