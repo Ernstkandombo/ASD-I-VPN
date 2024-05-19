96959b342b5b5238e152c0c0fdad5ddd730374ef
Ernst Kandombo <51041541+Ernstkandombo@users.noreply.github.com>
2024-05-20 00:15:25 +0200

    BB

--- src/components/sections/topbanner.js
+++ src/components/sections/topbanner.js
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-export default function topbanner() {
+export default function Topbanner() {
     useEffect(() => {
         const fetchData = async () => {
             try {
