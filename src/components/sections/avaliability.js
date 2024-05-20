cdcb85cf396db29902e78687be3730edb0ebde9a
Ernst Kandombo <51041541+Ernstkandombo@users.noreply.github.com>
2024-05-20 14:53:54 +0200

    ll

--- src/components/sections/avaliability.js
+++ src/components/sections/avaliability.js
@@ -6,7 +6,7 @@ export default function avaliability() {
     return (
         <section className="items-center justify-center flex flex-col py-16 bg-[#fc7219] text-center text-white ">
             <h2 className="text-2xl font-bold mb-4">Available in 6 countries now. More regions coming soon.</h2>
-            <Button className="bg-black text-white py-3 px-6 rounded-full">Join the Waitlist</Button>
+            <Button className="bg-black hover:bg-gray-800">Join the Waitlist</Button>
             <Image src="/assets/globe.png" width={600} height={600} alt="VPN illustration" className="w-full max-w-xs md:max-w-sm lg:max-w-md" />
         </section>
     )
